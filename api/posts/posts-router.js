6ef78c27d50625b1ab1f442c188b0d1958160dd4
Sallie McKee <salliemckee@Morrigan.local>
2022-04-12 15:57:06 -0500

    got post working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -34,7 +34,33 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// router.verb("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Posts.insert({ title, contents }).then(({ id }) => {
+      return Posts.findById(id)
+        .then((post) => {
+          res.status(201).json(post);
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).json({
+            message: "There was an error while saving the post to the database",
+          });
+        });
+    });
+    //   .catch((error) => {
+    //     console.log(error);
+    //     res.status(500).json({
+    //       message: "There was an error while saving the post to the database",
+    //     });
+    //   });
+  }
+});
 
 // router.verb("/", (req, res) => {});
 // router.verb("/", (req, res) => {});
