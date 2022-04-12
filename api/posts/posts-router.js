85c8d035e2db4c73e5c1df29de1810c46e45d1f3
Sallie McKee <salliemckee@Morrigan.local>
2022-04-12 15:30:47 -0500

    got first two get requests working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,5 +1,43 @@
 // implement your posts router here
 const express = require("express");
+const Posts = require("./posts-model");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  Posts.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
+    });
+});
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((id) => {
+      if (!id) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      } else {
+        res.status(200).json(id);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" });
+    });
+});
+
+// router.verb("/", (req, res) => {});
+
+// router.verb("/", (req, res) => {});
+// router.verb("/", (req, res) => {});
+// router.verb("/", (req, res) => {});
 
 module.exports = router;
